256d0328da456a49006a26dd5dac5ef65b41a16a
uttammagaju <108165184+uttammagaju@users.noreply.github.com>
2024-06-24 16:49:53 +0545

    add orderhead summery

--- ECommerceSite/Areas/Customers/Controllers/CartController.cs
+++ ECommerceSite/Areas/Customers/Controllers/CartController.cs
@@ -1,8 +1,11 @@
-﻿using ECommereceSiteData.Repository.IRepository;
+﻿using ECommerceSiteUtiltiy;
+using ECommereceSiteData.Repository.IRepository;
+using ECommereceSiteModels;
 using ECommereceSiteModels.Models;
 using ECommereceSiteModels.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ObjectPool;
 using System.Security.Claims;
 
 namespace ECommerceSite.Areas.Customers.Controllers
@@ -12,6 +15,7 @@ namespace ECommerceSite.Areas.Customers.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
         public ShoppingCartVM ShoppingCartVM { get; set; }
         public CartController(IUnitOfWork unitOfWork)
         {
@@ -37,16 +41,21 @@ namespace ECommerceSite.Areas.Customers.Controllers
 
         public IActionResult Summary()
         {
+            // Retrieve the user's ID from the claims identity
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            // Initialize the ShoppingCartVM with the user's shopping cart and an empty order header
             ShoppingCartVM = new()
             {
                 ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
                 OrderHeader = new()
             };
 
+            // Retrieve the user's details from the database and assign to the order header
             ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
 
+            // Populate the order header with the user's details
             ShoppingCartVM.OrderHeader.Name = ShoppingCartVM.OrderHeader.ApplicationUser.Name;
             ShoppingCartVM.OrderHeader.PhoneNumber = ShoppingCartVM.OrderHeader.ApplicationUser.PhoneNumber;
             ShoppingCartVM.OrderHeader.StreetAddress = ShoppingCartVM.OrderHeader.ApplicationUser.StreetAddress;
@@ -54,37 +63,29 @@ namespace ECommerceSite.Areas.Customers.Controllers
             ShoppingCartVM.OrderHeader.State = ShoppingCartVM.OrderHeader.ApplicationUser.State;
             ShoppingCartVM.OrderHeader.PostalCode = ShoppingCartVM.OrderHeader.ApplicationUser.PostalCode;
 
-
+            // Calculate the total order cost based on the items in the shopping cart
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
                 cart.Price = GetPriceBasedOnQuantity(cart);
                 ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
 
             }
+            // Return the view with the populated ShoppingCartVM
             return View(ShoppingCartVM);
         }
 
         [HttpPost]
         [ActionName("Summary")]
-        public IActionResult SummaryPost(ShoppingCartVM shoppingCartVM)
+        public IActionResult SummaryPost()
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-            ShoppingCartVM = new()
-            {
-                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product"),
-                OrderHeader = new()
-            };
+            ShoppingCartVM.ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product");
 
-            ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+            ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+            ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
 
-            ShoppingCartVM.OrderHeader.Name = ShoppingCartVM.OrderHeader.ApplicationUser.Name;
-            ShoppingCartVM.OrderHeader.PhoneNumber = ShoppingCartVM.OrderHeader.ApplicationUser.PhoneNumber;
-            ShoppingCartVM.OrderHeader.StreetAddress = ShoppingCartVM.OrderHeader.ApplicationUser.StreetAddress;
-            ShoppingCartVM.OrderHeader.City = ShoppingCartVM.OrderHeader.ApplicationUser.City;
-            ShoppingCartVM.OrderHeader.State = ShoppingCartVM.OrderHeader.ApplicationUser.State;
-            ShoppingCartVM.OrderHeader.PostalCode = ShoppingCartVM.OrderHeader.ApplicationUser.PostalCode;
-
+            ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
 
             foreach (var cart in ShoppingCartVM.ShoppingCartList)
             {
@@ -92,8 +93,47 @@ namespace ECommerceSite.Areas.Customers.Controllers
                 ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
 
             }
-            return View(ShoppingCartVM);
+            if(applicationUser.CompanyId.GetValueOrDefault() == 0)
+            {
+                //it is a regular custome account 
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusPending;
+            }
+            else
+            {
+                //it is a company user
+                ShoppingCartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+                ShoppingCartVM.OrderHeader.OrderStatus = SD.StatusApproval;
+            }
+            _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+            _unitOfWork.Save();
+            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                OrderDetail orderDetail = new()
+                {
+                    ProductId = cart.ProductId,
+                    OrderHeaderId = ShoppingCartVM.OrderHeader.Id,
+                    Price = cart.Price,
+                    Count = cart.Count,
+                };
+                _unitOfWork.OrderDetail.Add(orderDetail);
+                _unitOfWork.Save();
+             }
+
+            if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+                {
+                    //it is a regular custome account and we need to capture payment.
+                   //strip logic
+
+                }
+            return RedirectToAction(nameof(OrderConfirmation), new {id = ShoppingCartVM.OrderHeader.Id});
         }
+
+        public IActionResult OrderConfirmation(int id)
+        {
+            return View(id);
+        }
+
         public IActionResult Plus(int cartId)
         {
             var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
@@ -135,7 +175,7 @@ namespace ECommerceSite.Areas.Customers.Controllers
             double discountRate = double.Parse(shoppingCart.Product.discountRate);
 
             // Calculate discounted price if a discount rate is available
-            double discountedPrice = originalPrice - (originalPrice * discountRate / 100);
+            double discountedPrice = originalPrice - (originalPrice * (discountRate / 100));
 
             return discountedPrice;
         }
